048b5a9dc11d787a8d8489efec1d99a3f85e142d
daitken <daitken@discoverygarden.ca>
2013-03-05 11:37:29 -0400

    change so that it must be run as root

--- update.sh
+++ update.sh
@@ -1,10 +1,21 @@
-for f in */ ; do
-  cd $f
-  if [ -d .git ]
-   then
-    echo Checking for update in $f ...
-    git pull origin
-  fi
+#!/bin/bash
+
+if [ "$(id -u)" = "0" ]
+then
+  echo Getting updates ...;
+  for f in */ ; do
+    cd $f
+    if [ -d .git ]
+      then
+      echo Checking for update in $f ...;
+      git pull origin;
+    fi
   cd ..
-done
+  done
+  exit 1
+else
+  echo Must be root user to run updates. Try sudo ./update.sh;
+  exit 1;
+fi
+
 
